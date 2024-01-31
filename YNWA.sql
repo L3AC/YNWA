26addd4f9e5e62186406b8bc6cde4dc75af9605a
L3AC <101782981+L3AC@users.noreply.github.com>
2024-01-31 16:01:22 -0600

    Add files via upload

---
+++ YNWA.sql
@@ -0,0 +1,114 @@
+DROP DATABASE dbYNWA;
+CREATE DATABASE dbYNWA;
+use dbYNWA;
+
+/*PRC = TABLAS DINAMICAS */
+/*CTG = CATALOGOS */
+/*SEC = TABLAS DE SEGURIDAD*/
+
+CREATE TABLE sec_opciones(
+id_opc int,
+descripcion varchar(255),
+PRIMARY KEY (id_opc)
+);
+CREATE TABLE sec_roles(
+id_rol int,
+descripcion varchar(255),
+estado varchar(1),
+PRIMARY KEY (id_rol)
+);
+CREATE TABLE sec_opc_rol(
+id_opc_rol int,
+id_opc int,
+id_rol int,
+PRIMARY KEY (id_opc_rol),
+FOREIGN KEY(id_opc) REFERENCES sec_opciones(id_opc)
+ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(id_rol) REFERENCES sec_roles(id_rol)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE sec_usuarios(
+id_usuario INT,
+id_rol int,
+usuario varchar(30) UNIQUE,
+dui varchar(10)UNIQUE,
+clave varchar(30),
+nombres varchar(255),
+apellidos varchar(255),
+email varchar(100),
+pin varchar(6),
+estado varchar(1),
+PRIMARY KEY (id_usuario),
+FOREIGN KEY(id_rol) REFERENCES sec_roles(id_rol)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table prc_clientes(
+id_cliente INT,
+usuario varchar(30) UNIQUE,
+clave varchar(30),
+nombres varchar(255),
+apellidos varchar(255),
+email varchar(100),
+pin varchar(6),
+estado varchar(1),
+PRIMARY KEY(id_cliente)
+);
+
+create table ctg_marcas(
+id_marca int,
+descripcion varchar(255),
+estado varchar(1),
+PRIMARY KEY (id_marca)
+);
+
+CREATE TABLE ctg_modelos(
+id_modelo int,
+id_marca int,
+descripcion varchar(255),
+stock int,
+estado varchar(1),
+PRIMARY KEY(id_modelo),
+FOREIGN KEY(id_marca) REFERENCES ctg_marcas(id_marca)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+create table ctg_tallas(
+id_talla int,
+descripcion varchar(255),
+estado varchar(1),
+PRIMARY KEY (id_talla)
+);
+
+CREATE TABLE prc_modelo_tallas(
+id_modelotalla int,
+id_talla int,
+id_modelo int,
+precio float,
+primary key (id_modelotalla),
+FOREIGN KEY(id_modelo) REFERENCES ctg_modelos(id_modelo)
+ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(id_talla) REFERENCES ctg_tallas(id_talla)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+create table prc_pedidos(
+id_pedido int,
+id_cliente int,
+fecha datetime,
+estado varchar(2),
+PRIMARY KEY (id_pedido),
+FOREIGN KEY(id_cliente) REFERENCES prc_clientes(id_cliente)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+create table prc_detalle_pedidos(
+id_detalle int,
+id_pedido int,
+id_modelo int,
+cantidad int,
+PRIMARY KEY (id_detalle),
+FOREIGN KEY(id_pedido) REFERENCES prc_pedidos(id_pedido)
+ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY(id_modelo) REFERENCES ctg_modelos(id_modelo)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+

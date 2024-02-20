9e3bb4d9128978a345db6ea7c42b7509aa8d9815
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-02-19 21:29:39 -0600

    C

---
+++ api/models/handler/7comentarios_handler.php
@@ -0,0 +1,146 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once('../../helpers/database.php');
+/*
+*	Clase para manejar el comportamiento de los datos de la tabla PRODUCTO.
+*/
+class NoticiaHandler
+{
+    /*
+    *   Declaración de atributos para el manejo de datos.
+    */
+    protected $id = null;
+    protected $nombre = null;
+    protected $descripcion = null;
+    protected $precio = null;
+    protected $existencias = null;
+    protected $imagen = null;
+    protected $categoria = null;
+    protected $estado = null;
+
+    // Constante para establecer la ruta de las imágenes.
+    const RUTA_IMAGEN = '../../images/modelos/';
+
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
+    */
+    public function searchRows()
+    {
+        $value = '%' . Validator::getSearchValue() . '%';
+
+        $sql = 'SELECT mo.id_modelo, mo.descripcion,mo.foto, mo.estado,ma.descripcion as marca
+        FROM prc_modelos mo
+        INNER JOIN ctg_marcas ma USING(id_marca)
+        WHERE mo.descripcion LIKE ? OR ma.descripcion LIKE ?
+        ORDER BY mo.descripcion';
+
+        $params = array($value, $value);
+        return Database::getRows($sql, $params);
+    }
+
+    public function createRow()
+    {
+        $sql = 'INSERT INTO producto(nombre_producto, descripcion_producto, precio_producto, existencias_producto, imagen_producto, estado_producto, id_categoria, id_administrador)
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
+        $params = array($this->nombre, $this->descripcion, $this->precio, $this->existencias, $this->imagen, $this->estado, $this->categoria, $_SESSION['idAdministrador']);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readAll()
+    {
+        $sql = 'select n.id_noticia,n.id_tiponoticia,n.titulo,n.foto,n.contenido,n.estado,
+        DATE_FORMAT(n.fecha, "%d-%m-%Y") AS fecha from prc_noticias n
+        INNER JOIN ctg_tiponoticias tn USING(id_tiponoticia)
+        ORDER BY n.titulo';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql ='select n.id_noticia,n.id_tiponoticia,n.titulo,n.foto,n.contenido,n.estado,
+        DATE_FORMAT(n.fecha, "%d-%m-%Y") AS fecha from prc_noticias n
+        INNER JOIN ctg_tiponoticias tn USING(id_tiponoticia)
+        WHERE n.id_noticia=?
+        ORDER BY n.titulo';
+        $params = array($this->id);
+        $data = Database::getRow($sql, $params);
+        return $data;
+    }
+
+    public function readFilename()
+    {
+        $sql = 'SELECT foto
+                FROM prc_modelos
+                WHERE id_modelo = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow()
+    {
+        $sql = 'UPDATE prc_modelo 
+                SET foto = ?, descripcion = ?,estado = ?, id_marca = ?
+                WHERE id_modelo = ?';
+        $params = array($this->imagen, $this->nombre,$this->estado, $this->categoria, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM prc_modelos
+                WHERE id_modelo = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readProductosCategoria()
+    {
+        $sql = 'SELECT mo.id_modelo, mo.descripcion,mo.foto, mo.estado,ma.descripcion as marca
+        FROM prc_modelos mo
+        INNER JOIN ctg_marcas ma USING(id_marca)
+        WHERE mo.id_marca LIKE ? OR estado="A"
+        ORDER BY mo.descripcion';
+        /*'SELECT id_producto, imagen_producto, nombre_producto, descripcion_producto, precio_producto, existencias_producto
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                WHERE id_categoria = ? AND estado_producto = true
+                ORDER BY nombre_producto'*/;
+        $params = array($this->categoria);
+        return Database::getRows($sql, $params);
+    }
+
+    /*
+    *   Métodos para generar gráficos.
+    */
+    public function cantidadProductosCategoria()
+    {
+        $sql = 'SELECT nombre_categoria, COUNT(id_producto) cantidad
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                GROUP BY nombre_categoria ORDER BY cantidad DESC LIMIT 5';
+        return Database::getRows($sql);
+    }
+
+    public function porcentajeProductosCategoria()
+    {
+        $sql = 'SELECT nombre_categoria, ROUND((COUNT(id_producto) * 100.0 / (SELECT COUNT(id_producto) FROM producto)), 2) porcentaje
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                GROUP BY nombre_categoria ORDER BY porcentaje DESC';
+        return Database::getRows($sql);
+    }
+
+    /*
+    *   Métodos para generar reportes.
+    */
+    public function productosCategoria()
+    {
+        $sql = 'SELECT nombre_producto, precio_producto, estado_producto
+                FROM producto
+                INNER JOIN categoria USING(id_categoria)
+                WHERE id_categoria = ?
+                ORDER BY nombre_producto';
+        $params = array($this->categoria);
+        return Database::getRows($sql, $params);
+    }
+}

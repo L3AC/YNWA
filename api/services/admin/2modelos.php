ac9434fd4c2b25b726bc64622503ca2af4f0aeca
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-02-27 22:15:53 -0600

    C

--- api/services/admin/2modelos.php
+++ api/services/admin/2modelos.php
@@ -47,7 +47,7 @@ if (isset($_GET['action'])) {
                     $result['status'] = 1;
                     $result['message'] = 'Existen ' . count($result['dataset']) . ' registros';
                 } else {
-                    $result['error'] = ' no existen registrosregistrados';
+                    $result['error'] = ' No se encontraron registros';
                 }
                 break;
             case 'readsubAll':

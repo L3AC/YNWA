b1e2cec5a24646eb8b3e9ad645e8e68c127747fb
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-03-11 23:29:07 -0600

    C

--- controllers/public/detail.js
+++ controllers/public/detail.js
@@ -151,66 +151,6 @@ const openComentario = async (id) => {
     if (DATA.status) {
         // Se muestra la caja de diálogo con su título.
         SAVE_MODAL2.show();
-       /* DATA.dataset.forEach(row => {
-
-            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
-            LISTCOMENTARIO.innerHTML += `
-            <li class="list-group-item">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h5>${row.cliente}</h5>
-                    </div>
-                    <div class="col-md-4">
-
-
-                    <div class="rating">
-                        <input type="radio" id="star-1" name="star-radio" value="star-1" data-rating="1">
-                        <label for="star-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-                        </label>
-                        <input type="radio" id="star-2" name="star-radio" value="star-1" data-rating="2">
-                        <label for="star-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-                        </label>
-                        <input type="radio" id="star-3" name="star-radio" value="star-1" data-rating="3">
-                        <label for="star-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-                        </label>
-                        <input type="radio" id="star-4" name="star-radio" value="star-1" data-rating="4">
-                        <label for="star-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-                        </label>
-                        <input type="radio" id="star-5" name="star-radio" value="star-1" data-rating="5">
-                        <label for="star-5">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-                        </label>
-                    </div>
-
-
-
-                    </div>
-                    <div class="col-md-12">
-                        <textarea class="form-control">${row.contenido_comentario}</textarea>
-                    </div>
-                </div>
-                <p class="mt-2">${row.fecha_comentario}</p>
-            </li>
-            
-            `;
-            let ratingValue = parseInt(row.puntuacion_comentario);
-            let stars = document.querySelectorAll('.rating input[type="radio"]');
-            stars.forEach((star, index) => {
-                if (index < 6- ratingValue) {
-                    star.checked = true;
-                } else {
-                    star.checked = false;
-                }
-            });
-
-        });
-        document.querySelectorAll('.rating input[type="radio"], .rating label').forEach(function (element) {
-            element.disabled = true;
-        });*/
         DATA.dataset.forEach(row => {
             // Se crea y concatena la fila de la tabla con los datos de cada registro.
             let comentario = `

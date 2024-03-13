2a8affd1dcc0bcf549dc5d81b23a68435f994ecf
FranciscoGarc <20220326@ricaldone.edu.sv>
2024-03-12 21:55:01 -0600

    c

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -30,26 +30,44 @@ const loadTemplate = async () => {
         if (!location.pathname.endsWith('login.html')) {
             // Se agrega el encabezado de la página web antes del contenido principal.
             MAIN.insertAdjacentHTML('beforebegin', `
-                <header >
-                    <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #CCC8AA;" >
-                        <div class="container">
-                            <a class="navbar-brand" href="index.html"><img src="../../resources/img/logo.png" height="60" alt="YNWA" ></a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <header>
+        <nav class="navbar navbar-expand-lg  fixed-top" style="background-color: #CCC8AA; ">
+            <div class="container">
+                    <!-- Left elements -->
+                    <div class="col-md-4 d-flex justify-content-center justify-content-md-start mb-3 mb-md-0">
+                        <!-- Logo -->
+                        <a class="navbar-brand" href="index.html"><img src="../../resources/img/logo.png" height="60"
+                                alt="YNWA"></a>
+                    </div>
+                    <!-- Left elements -->
+
+                    <!-- Center elements -->
+                    <div class="col-md-3">
+                        <div class="nav-link">
+                            <div class="input-group">
+                                <input id="searchMain" type="text" name="searchMain" class="form-control"
+                                    placeholder="Búsqueda">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Center elements -->
+
+                    <!-- Right elements -->
+                    <div class="col-md-5 d-flex justify-content-center justify-content-md-end align-items-center">
+                        <div class="d-flex">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                                aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                                 <div class="navbar-nav ms-auto">
-                                
-                                <div class="col-3 nav-link">
-                                    <div class="input-group">
-                                        <input id="searchMain" type="text" name="searchMain" class="form-control" placeholder="Búsqueda">
-                                    </div>
-                                </div>
-                                
                                     <li class="nav-item dropdown ">
-                                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false"
-                                         href="index.html"><i class="bi bi-tags-fill"></i> Marcas</a>
+                                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+                                            aria-expanded="false" href="index.html"><i class="bi bi-tags-fill"></i>
+                                            Marcas</a>
                                         <ul class="dropdown-menu" id="listmarca">
+
                                         </ul>
                                     </li>
                                     <a class="nav-link" href="cart.html"><i class="bi bi-cart"></i> Carrito</a>
@@ -59,8 +77,11 @@ const loadTemplate = async () => {
                                 </div>
                             </div>
                         </div>
-                    </nav>
-                </header>
+                    </div>
+                    <!-- Right elements -->
+            </div>
+        </nav>
+    </header>
             `);
 
             const SEARCH_MAIN = document.getElementById('searchMain');

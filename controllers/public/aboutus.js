1dc9b084b33433f37d3b6d635bdb3fcd6247f8ea
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-03-18 14:26:51 -0600

    C

---
+++ controllers/public/aboutus.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = 'Acerca de nosotros';
+});

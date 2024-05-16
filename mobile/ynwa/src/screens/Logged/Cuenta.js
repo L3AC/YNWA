82b97fd71920dea44a4bb279186385abacc555f3
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-05-16 11:07:44 -0600

    update Cuenta.js

--- mobile/ynwa/src/screens/Logged/Cuenta.js
+++ mobile/ynwa/src/screens/Logged/Cuenta.js
@@ -3,7 +3,7 @@ import { View, Button ,Text} from 'react-native';
 
 const Cuenta = ({ navigation }) => (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Pantalla de Home</Text>
+    <Text>Pantalla de Cuenta</Text>
   </View>
 );
 

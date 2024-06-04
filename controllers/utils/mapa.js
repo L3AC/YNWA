5e9dd193a819e76b9c00955e93be12e76c9ad089
rodri <alvaarodrigo@gmail.com>
2024-06-04 10:45:07 -0600

    Mapa

--- controllers/utils/mapa.js
+++ controllers/utils/mapa.js
@@ -72,6 +72,18 @@ function habilitarModoSatelite() {
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles © Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     }).addTo(mapa);
+    
+    mapa.on('click', function(e) {
+        if (marcador) {
+            mapa.removeLayer(marcador);
+        }
+
+        var latitud = e.latlng.lat;
+        var longitud = e.latlng.lng;
+        mostrarUbicacion(latitud, longitud);
+        obtenerNombreLugar(latitud, longitud);
+        document.getElementById('direccionCliente').value = 'Latitud: ' + latitud + ', Longitud: ' + longitud;
+    });
 }
 
 function habilitarVerCalles() {
@@ -82,6 +94,18 @@ function habilitarVerCalles() {
     mapa = L.map('mapa').setView([13.6929, -89.2182], 13); // San Salvador, El Salvador
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapa);
+
+    mapa.on('click', function(e) {
+        if (marcador) {
+            mapa.removeLayer(marcador);
+        }
+
+        var latitud = e.latlng.lat;
+        var longitud = e.latlng.lng;
+        mostrarUbicacion(latitud, longitud);
+        obtenerNombreLugar(latitud, longitud);
+        document.getElementById('direccionCliente').value = 'Latitud: ' + latitud + ', Longitud: ' + longitud;
+    });
 }
 
 inicializarMapa(); // Llamar a la función inicializarMapa() al cargar la página

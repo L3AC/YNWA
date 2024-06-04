182383b2333e5959b88a67310c5bf713e536da2d
rodri <alvaarodrigo@gmail.com>
2024-06-04 10:03:48 -0600

    Cambios de mapa

--- controllers/utils/mapa.js
+++ controllers/utils/mapa.js
@@ -1,59 +1,87 @@
-var mapa = L.map('mapa').setView([13.6929, -89.2182], 13); // San Salvador, El Salvador
-            var marcador;
+var mapa;
+var marcador;
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapa);
+function inicializarMapa() {
+    mapa = L.map('mapa').setView([13.6929, -89.2182], 13); // San Salvador, El Salvador
 
-            mapa.on('click', function (e) {
-                if (marcador) {
-                    mapa.removeLayer(marcador);
-                }
+    habilitarVerCalles(); // Establecer el mapa en el modo de calles al cargar
 
-                var latitud = e.latlng.lat;
-                var longitud = e.latlng.lng;
+    mapa.on('click', function(e) {
+        if (marcador) {
+            mapa.removeLayer(marcador);
+        }
+
+        var latitud = e.latlng.lat;
+        var longitud = e.latlng.lng;
+        mostrarUbicacion(latitud, longitud);
+        obtenerNombreLugar(latitud, longitud);
+        document.getElementById('direccionCliente').value = 'Latitud: ' + latitud + ', Longitud: ' + longitud;
+    });
+}
+
+function mostrarUbicacion(latitud, longitud) {
+    marcador = L.marker([latitud, longitud]).addTo(mapa);
+}
+
+function obtenerNombreLugar(latitud, longitud) {
+    fetch('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + latitud + '&lon=' + longitud)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            document.getElementById('direccionCliente').value = data.display_name;
+        })
+        .catch(function(error) {
+            console.error('Error al obtener el nombre del lugar:', error);
+        });
+}
+
+function buscarDireccion() {
+    var direccion = document.getElementById('direccionCliente').value;
+
+    fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(direccion))
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            if (data.length > 0) {
+                var latitud = parseFloat(data[0].lat);
+                var longitud = parseFloat(data[0].lon);
+                mapa.setView([latitud, longitud], 15);
                 mostrarUbicacion(latitud, longitud);
-                obtenerNombreLugar(latitud, longitud);
-                document.getElementById('direccionCliente').value = 'Latitud: ' + latitud + ', Longitud: ' + longitud;
-            });
-
-            function mostrarUbicacion(latitud, longitud) {
-                marcador = L.marker([latitud, longitud]).addTo(mapa);
+            } else {
+                alert('No se encontró la dirección.');
             }
+        })
+        .catch(function(error) {
+            console.error('Error al buscar la dirección:', error);
+        });
+}
 
-            function obtenerNombreLugar(latitud, longitud) {
-                fetch('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + latitud + '&lon=' + longitud)
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        document.getElementById('direccionCliente').value = data.display_name;
-                    })
-                    .catch(function (error) {
-                        console.error('Error al obtener el nombre del lugar:', error);
-                    });
-            }
+function limpiarInput() {
+    document.getElementById('direccionCliente').value = '';
+}
 
-            function buscarDireccion() {
-                var direccion = document.getElementById('direccionCliente').value;
+function habilitarModoSatelite() {
+    if (mapa) {
+        mapa.remove();
+    }
 
-                fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(direccion))
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        if (data.length > 0) {
-                            var latitud = parseFloat(data[0].lat);
-                            var longitud = parseFloat(data[0].lon);
-                            mapa.setView([latitud, longitud], 15);
-                            mostrarUbicacion(latitud, longitud);
-                        } else {
-                            alert('No se encontró la dirección.');
-                        }
-                    })
-                    .catch(function (error) {
-                        console.error('Error al buscar la dirección:', error);
-                    });
-            }
+    mapa = L.map('mapa').setView([13.6929, -89.2182], 13); // San Salvador, El Salvador
 
-            function limpiarInput() {
-                document.getElementById('direccionCliente').value = '';
-            }
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles © Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    }).addTo(mapa);
+}
+
+function habilitarVerCalles() {
+    if (mapa) {
+        mapa.remove();
+    }
+
+    mapa = L.map('mapa').setView([13.6929, -89.2182], 13); // San Salvador, El Salvador
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapa);
+}
+
+inicializarMapa(); // Llamar a la función inicializarMapa() al cargar la página

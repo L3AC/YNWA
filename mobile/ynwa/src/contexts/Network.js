7f816d2f26172e0ae140300362543588595dd60b
L3AC <leac.2xy@gmail.com>
2024-08-15 21:52:47 -0600

    update Network.js

--- mobile/ynwa/src/contexts/Network.js
+++ mobile/ynwa/src/contexts/Network.js
@@ -1,2 +1,3 @@
 //export const SERVER = "http://10.10.2.25:80//YNWA/api/";
-export const SERVER = "http://192.168.1.9:80//YNWA/api/";
+//export const SERVER = "http://192.168.1.9:80//YNWA/api/";
+export const SERVER = "https://ynwa.000.pe/YNWA/api/";
